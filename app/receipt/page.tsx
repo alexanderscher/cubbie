b4f7f9a740d54951dc4e6777c1bdbe0d3cfb6a7b
alexanderscher <alexcscher@gmail.com>
2024-01-26 19:12:50 -0800

    update

--- app/receipt/page.tsx
+++ app/receipt/page.tsx
@@ -5,47 +5,47 @@ const page = () => {
   const dataArray = [1, 2, 3, 4, 5];
 
   return (
-    <div className="reciepts ">
-      <div className="flex flex-col gap-4 reciept-bar">
+    <div className="receipts ">
+      <div className="flex flex-col gap-4 receipt-bar">
         <h1 className="text-green-900 text-2xl ">Macys</h1>
-        <div className="reciept-info">
+        <div className="receipt-info">
           <h1 className="text-slate-500">Order Number</h1>
           <h1 className="">123123123</h1>
         </div>
 
         <div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Store</h1>
             <h1 className="">Macys</h1>
           </div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Address</h1>
             <h1 className="">1234 12th street 90077</h1>
           </div>
         </div>
         <div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Number of items</h1>
             <h1 className="">5</h1>
           </div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Total Amount</h1>
             <h1 className="">$300.00</h1>
           </div>
         </div>
         <div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Date Ordered</h1>
             <h1 className="">12/23/23</h1>
           </div>
-          <div className="reciept-info">
+          <div className="receipt-info">
             <h1 className="text-slate-500">Return Date</h1>
             <h1 className="">12/30/23</h1>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-10 reciept-grid">
+      <div className="grid grid-cols-3 gap-10 receipt-grid">
         {dataArray.map((item, index) => (
           <div key={index}>
             <ReceiptItems />

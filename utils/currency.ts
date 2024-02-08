8f9f9d0eeee5fbe4dd4853eaf461538d16c595a1
alexanderscher <alexcscher@gmail.com>
2024-02-08 15:19:55 -0800

    update

--- utils/currency.ts
+++
@@ -1,9 +0,0 @@
-export const formatCurrencyValue = (value) => {
-  // Remove any non-numeric characters except for the decimal point
-  const numericValue = value.replace(/[^\d.]/g, "");
-
-  // Convert to a number and format as a fixed two-decimal place string
-  const formattedValue = `$${parseFloat(numericValue).toFixed(2)}`;
-
-  return formattedValue;
-};

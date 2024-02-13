22b33035bc08b8f783a4178be2b1140b70fe1a22
alexanderscher <alexcscher@gmail.com>
2024-02-13 14:42:31 -0800

    update

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,100 +1,101 @@
-// import { ourFileRouter } from "@/app/api/uploadthing/core";
-// import { ItemInput } from "@/types/formTypes/form";
-// import { useUploadThing } from "@/utils/uploadthing";
-// import { NextResponse } from "next/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+import prisma from "@/prisma/client";
+import { ItemInput } from "@/types/formTypes/form";
+import { useUploadThing } from "@/utils/uploadthing";
+import { NextResponse } from "next/server";
 
-// export async function POST(request: Request) {
-//   const { startUpload, permittedFileInfo } = useUploadThing("imageUploader", {
-//     onClientUploadComplete: (res) => {
-//       (async () => {})().catch(console.error);
-//     },
-//   });
-//   try {
-//     const json = await request.json();
+export async function POST(request: Request) {
+  const { startUpload, permittedFileInfo } = useUploadThing("imageUploader", {
+    onClientUploadComplete: (res) => {
+      (async () => {})().catch(console.error);
+    },
+  });
+  try {
+    const json = await request.json();
 
-//     const {
-//       type,
-//       store,
-//       card,
-//       amount,
-//       trackingNumber,
-//       boughtDate,
-//       daysUntilReturn,
-//       finalReturnDate,
-//       receiptImage,
-//       receiptImageFile,
-//       items,
-//     } = json;
+    const {
+      type,
+      store,
+      card,
+      amount,
+      trackingNumber,
+      boughtDate,
+      daysUntilReturn,
+      finalReturnDate,
+      receiptImage,
+      receiptImageFile,
+      items,
+    } = json;
 
-//     const requiredFields = ["type", "store", "amount", "items"];
+    const requiredFields = ["type", "store", "amount", "items"];
 
-//     const missingFields: string[] = [];
+    const missingFields: string[] = [];
 
-//     for (const field of requiredFields) {
-//       if (!json[field]) {
-//         missingFields.push(field);
-//       }
-//     }
+    for (const field of requiredFields) {
+      if (!json[field]) {
+        missingFields.push(field);
+      }
+    }
 
-//     if (missingFields.length > 0) {
-//       return new NextResponse(
-//         JSON.stringify({
-//           error: `Missing or invalid fields: ${missingFields.join(", ")}`,
-//         }),
-//         {
-//           status: 400,
-//           headers: { "Content-Type": "application/json" },
-//         }
-//       );
-//     }
+    if (missingFields.length > 0) {
+      return new NextResponse(
+        JSON.stringify({
+          error: `Missing or invalid fields: ${missingFields.join(", ")}`,
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
 
-//     const itemsArray = items.map((item: ItemInput) => {
-//       return {
-//         description: item.description,
-//         photo: item.photo,
-//         photoFile: item.photoFile, // Ensure this exists in your item structure
-//         price: item.price,
-//         barcode: item.barcode,
-//         asset: item.asset,
-//         character: item.character,
-//         product_id: item.product_id,
-//       };
-//     });
+    const itemsArray = items.map((item: ItemInput) => {
+      return {
+        description: item.description,
+        photo: item.photo,
+        photoFile: item.photoFile, // Ensure this exists in your item structure
+        price: item.price,
+        barcode: item.barcode,
+        asset: item.asset,
+        character: item.character,
+        product_id: item.product_id,
+      };
+    });
 
-//     const receipt = await prisma.receipt.create({
-//       data: {
-//         type,
-//         store,
-//         card,
-//         amount,
-//         trackingNumber,
-//         boughtDate,
-//         daysUntilReturn,
-//         finalReturnDate,
-//         receiptImage,
-//         items: {
-//           create: itemsArray,
-//         },
-//       },
-//     });
+    const receipt = await prisma.receipt.create({
+      data: {
+        type,
+        store,
+        card,
+        amount,
+        tracking_number: trackingNumber,
+        purchase_date: boughtDate,
+        days_until_return: daysUntilReturn,
+        final_return_date: finalReturnDate,
+        receipt_image: receiptImage,
+        items: {
+          create: itemsArray,
+        },
+      },
+    });
 
-//     if (
-//       receiptImageFile &&
-//       receiptImageFile instanceof File &&
-//       permittedFileInfo &&
-//       receiptImage
-//     ) {
-//       startUpload([receiptImageFile]);
-//     }
+    if (
+      receiptImageFile &&
+      receiptImageFile instanceof File &&
+      permittedFileInfo &&
+      receiptImage
+    ) {
+      startUpload([receiptImageFile]);
+    }
 
-//     return new NextResponse(JSON.stringify(receipt), {
-//       status: 201,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (err) {
-//     return new NextResponse(JSON.stringify({ error: "Database error" }), {
-//       status: 500,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   }
-// }
+    return new NextResponse(JSON.stringify(receipt), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    return new NextResponse(JSON.stringify({ error: "Database error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}

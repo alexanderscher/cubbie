1acd56713498987b10bd47cb5cfa0b06c9d933bf
alexanderscher <alexcscher@gmail.com>
2024-02-25 09:34:14 -0800

    update

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -42,17 +42,27 @@ const Item = ({ item }: Props) => {
             Return by {formatDateToMMDDYY(item.receipt.return_date)}
           </p>
 
-          <div className="border-t-[1px] border-slate-300 flex flex-col  text-xs">
-            <TruncateText
-              text={item.receipt.store}
-              maxLength={15}
-              styles={""}
-            />
-
-            <p className="">{formatCurrency(item.price)}</p>
-          </div>
-          <div className="">
-            {item.barcode && <p className="text-xs">{item.barcode}</p>}
+          <div className="border-t-[1px] border-slate-300 flex flex-col  gap-1 text-xs">
+            <div className="mt-2">
+              <p className="text-slate-500">Store</p>
+              <Link href={`/receipt/${item.receipt_id}`} className="">
+                <TruncateText
+                  text={item.receipt.store}
+                  maxLength={15}
+                  styles={""}
+                />
+              </Link>
+            </div>
+            <div>
+              <p className="text-slate-500  ">Price</p>
+              <p className="">{formatCurrency(item.price)}</p>
+            </div>
+            {item.barcode && (
+              <div className="">
+                <p className="text-slate-500  ">Barcode</p>
+                <p className="text-xs">{item.barcode}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- app/components/receiptComponents/Receipt.tsx
+++ app/components/receiptComponents/Receipt.tsx
@@ -35,23 +35,29 @@ const Receipt = ({ receipt }: ReceiptProps) => {
             styles={"text-orange-600 text-xs sm:text-lg"}
           />
         </Link>
+        <div className="flex gap-1 text-sm">
+          <p className=" ">
+            {receipt.items.length}{" "}
+            {receipt.items.length === 1 ? "item" : "items"} |
+          </p>
+          <p className=" ">
+            {formatCurrency(
+              receipt.items.reduce((acc: number, curr: Item) => {
+                return acc + curr.price;
+              }, 0)
+            )}
+          </p>
+        </div>
 
-        <p className="text-slate-500 text-xs ">
-          Return by {formatDateToMMDDYY(receipt.return_date)}
-        </p>
+        <div className="border-t-[1px] border-slate-300  flex flex-col text-xs sm:text-sm gap-1">
+          <div className="text-xs mt-2">
+            <p className="text-slate-500  ">Purchased date</p>
+            <p> {formatDateToMMDDYY(receipt.purchase_date)}</p>
+          </div>
 
-        <div className="">
-          <div className="border-t-[1px] border-slate-300  flex flex-col text-xs sm:text-sm">
-            <div className="flex gap-1 mt-2">
-              <p className=" ">{receipt.items.length} items | </p>
-              <p className=" ">
-                {formatCurrency(
-                  receipt.items.reduce((acc: number, curr: Item) => {
-                    return acc + curr.price;
-                  }, 0)
-                )}
-              </p>
-            </div>
+          <div className="text-xs">
+            <p className="text-slate-500  ">Return date</p>
+            <p> {formatDateToMMDDYY(receipt.purchase_date)}</p>
           </div>
         </div>
       </div>

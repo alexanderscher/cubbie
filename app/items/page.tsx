80f6fe65b00731edab0622f038eed06ba2073286
alexanderscher <alexcscher@gmail.com>
2024-02-26 17:08:31 -0800

    update

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,16 +1,16 @@
 import Header from "@/app/components/Header";
 import Items from "@/app/components/Home/Items";
-import {
-  SearchItemContext,
-  SearchItemProvider,
-} from "@/app/components/context/SearchtemContext";
+import { SearchItemProvider } from "@/app/components/context/SearchtemContext";
+import { Suspense } from "react";
 
 const HomeItems = () => {
   return (
     <div>
       <SearchItemProvider>
         <Header type="Items" />
-        <Items />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Items />
+        </Suspense>
       </SearchItemProvider>
     </div>
   );

b35872c825c6fab8f4f365ca040d18baac3c8f23
alexanderscher <alexcscher@gmail.com>
2024-03-01 14:06:56 -0800

    update

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,8 +1,32 @@
+"use client";
 import RegularButton from "@/app/components/buttons/RegularButton";
+import Receipt from "@/app/components/receiptComponents/Receipt";
+import { Project, Receipt as ReceiptType } from "@/types/receipt";
+import { formatDateToMMDDYY } from "@/utils/Date";
+import { create } from "domain";
+import { get } from "http";
 import Link from "next/link";
-import React from "react";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const ProjectID = () => {
+  const { id } = useParams();
+  const [project, setProject] = useState({
+    name: "",
+    created_at: "",
+    receipts: [],
+  });
+
+  useEffect(() => {
+    const getProject = async () => {
+      const res = await fetch(`/api/project/${id}`);
+      const data = await res.json();
+
+      setProject(data.project);
+    };
+    getProject();
+  }, [id]);
+
   return (
     <div>
       <div className="flex justify-between items-center gap-4 border-b-[1px] border-emerald-900 pb-4">
@@ -13,18 +37,36 @@ const page = () => {
             </p>
           </Link>
           <p className="text-emerald-900 text-sm">/</p>
-          {/* <p className="text-emerald-900 text-sm">{receipt.store}</p> */}
+          <p className="text-emerald-900 text-sm">{project.name}</p>
         </div>
-
-        <RegularButton
-          href="/receipt-type"
-          styles="bg border-emerald-900 text-emerald-900"
-        >
-          <p className="text-xs">Create new</p>
-        </RegularButton>
+        <div></div>
+      </div>
+      <div className="flex flex-col gap-8 mt-10">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-2xl text-orange-600">{project.name}</h1>
+            <p>Created on {formatDateToMMDDYY(project.created_at)}</p>{" "}
+          </div>
+          <div className="flex items-center gap-3">
+            <RegularButton styles="bg border-emerald-900 text-emerald-900">
+              <p className="text-xs">Edit</p>
+            </RegularButton>
+            <RegularButton
+              href="/receipt-type"
+              styles="bg border-emerald-900 text-emerald-900"
+            >
+              <p className="text-xs">Add receipt</p>
+            </RegularButton>
+          </div>
+        </div>
+        <div className="boxes">
+          {project.receipts.map((receipt: ReceiptType) => (
+            <Receipt key={receipt.id} receipt={receipt} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default ProjectID;

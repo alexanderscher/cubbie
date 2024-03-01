a3097e6614a1a0da25ac407fdedf0852824caa01
alexanderscher <alexcscher@gmail.com>
2024-02-29 20:47:30 -0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Projects from "@/app/components/Home/Projects";
 import Header from "@/app/components/headers/Header";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex flex-col pb-[400px]">
-      <Header type="Projects" />
-      <Projects />
+      <Suspense fallback={<div>Loading</div>}>
+        <Header type="Projects" />
+        <Projects />
+      </Suspense>
     </main>
   );
 }

1e0cd7fac4723892f9f7ac4efce0ed29f22e3988
alexanderscher <alexcscher@gmail.com>
2024-03-04 18:27:56 -0800

    update

--- app/components/navbar/HeaderItemNav.tsx
+++ app/components/navbar/HeaderItemNav.tsx
@@ -11,10 +11,10 @@ const HeaderNav = ({ item }: HeaderNavProps) => {
   return (
     <div className="flex flex-wrap justify-between items-center gap-4 border-b-[1px] border-emerald-900 pb-4">
       <div className="flex gap-4">
-        <Link href="/">
+        {/* <Link href="/">
           <p className="text-emerald-900 hover:text-orange-600 text-sm">Home</p>
-        </Link>
-        <p className="text-emerald-900 text-sm">/</p>
+        </Link> */}
+        {/* <p className="text-emerald-900 text-sm">/</p> */}
         {item.receipt.project && (
           <div className="flex">
             <Link href={`/project/${item.receipt.project.id}`}>
@@ -35,13 +35,6 @@ const HeaderNav = ({ item }: HeaderNavProps) => {
         <p className="text-emerald-900 text-sm">/</p>
         <p className="text-emerald-900 text-sm">{item.description}</p>
       </div>
-
-      <RegularButton
-        href="/receipt-type"
-        styles="bg border-emerald-900 text-emerald-900"
-      >
-        <p className="text-xs">Create new</p>
-      </RegularButton>
     </div>
   );
 };
--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -25,74 +25,81 @@ const ItemID = () => {
 
   if (!item.receipt) return <div className="min-h-screen">Loading</div>;
   return (
-    <div className="flex flex-col gap-6  max-w-[600px] ">
-      <HeaderItemNav item={item} />
+    <div className="w-full flex justify-center items-center">
+      <div className="flex flex-col gap-6  w-full max-w-[600px] ">
+        <HeaderItemNav item={item} />
 
-      <div className="w-full flex flex-col gap-4">
-        <RegularButton styles="bg-emerald-900" href={`/item/${id}/edit`}>
-          <p className="text-white text-xs">Edit</p>
-        </RegularButton>
-        {item.photo_url && (
-          <div className="w-full   ">
-            <div className="max-h-[300px] w-full overflow-hidden rounded-sm">
-              <Image
-                src={item.photo_url}
-                width={600}
-                height={300}
-                alt="Receipt Image"
-                className="w-full h-full object-cover cursor-pointer"
-                onClick={() => setIsOpen(true)}
-              />
-            </div>
-            <ImageModal
-              isOpen={isOpen}
-              setIsOpen={setIsOpen}
-              imageUrl={item.photo_url}
-              altText="Your Image Description"
-            />
-          </div>
-        )}
-        {!item.photo_url && (
-          <div className="max-h-[300px] w-full overflow-hidden rounded-sm">
-            <div className="w-full h-[110px] overflow-hidden relative flex justify-center items-center bg-slate-100 ">
-              <div className="w-full h-full flex justify-center items-center">
+        <div className="w-full flex flex-col gap-4">
+          <RegularButton styles="bg-emerald-900" href={`/item/${id}/edit`}>
+            <p className="text-white text-xs">Edit</p>
+          </RegularButton>
+          {item.photo_url && (
+            <div className="w-full   ">
+              <div className="max-h-[300px] w-full overflow-hidden rpunded-md">
                 <Image
-                  src="/item_b.png"
-                  alt=""
-                  width={60}
-                  height={60}
-                  className="object-cover "
-                  style={{ objectFit: "cover", objectPosition: "center" }}
+                  src={item.photo_url}
+                  width={600}
+                  height={300}
+                  alt="Receipt Image"
+                  className="w-full h-full object-cover cursor-pointer"
+                  onClick={() => setIsOpen(true)}
                 />
               </div>
+              <ImageModal
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                imageUrl={item.photo_url}
+                altText="Your Image Description"
+              />
             </div>
+          )}
+          <div>
+            <p className="text-xl text-orange-600">{item.description}</p>
           </div>
-        )}
-      </div>
-      <div>
-        <p className="text-xl text-orange-600">{item.description}</p>
-      </div>
+          {!item.photo_url && (
+            <div className="max-h-[300px] w-full overflow-hidden rounded-md">
+              <div className="w-full h-[110px] overflow-hidden relative flex justify-center items-center bg-slate-100 ">
+                <div className="w-full h-full flex justify-center items-center">
+                  <Image
+                    src="/item_b.png"
+                    alt=""
+                    width={60}
+                    height={60}
+                    className="object-cover "
+                    style={{ objectFit: "cover", objectPosition: "center" }}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
 
-      {item.receipt.asset_amount && item.receipt.asset_amount < item.price && (
-        <p className="text-md text-emerald-900">Asset</p>
-      )}
+        {item.receipt.asset_amount &&
+          item.receipt.asset_amount < item.price && (
+            <p className="text-md text-emerald-900">Asset</p>
+          )}
 
-      <div className="flex flex-col gap-4">
-        <div className="w-full  border-slate-400 border-b-[1px]   pb-2">
-          <p className="text-xs text-slate-400">Price</p>
-          <p>{formatCurrency(item.price)}</p>
-        </div>
-        <div className="w-full  border-slate-400 border-b-[1px]   pb-2">
-          <p className="text-xs text-slate-400">Barcode</p>
-          <p>{item.barcode ? item.barcode : "None"}</p>
-        </div>
-        <div className="w-full  border-slate-400 border-b-[1px]   pb-2">
-          <p className="text-xs text-slate-400">Character</p>
-          <p>{item.character ? item.character : "None"}</p>
-        </div>
-        <div className="w-full  border-slate-400 border-b-[1px]   pb-2">
-          <p className="text-xs text-slate-400">Product ID</p>
-          <p>{item.product_id ? item.product_id : "None"}</p>
+        <div className="flex flex-col gap-4">
+          <div className="w-full   bg-white rounded-md shadow p-2   pb-2">
+            <p className="text-xs text-slate-400">Store</p>
+            <p>{item.receipt.store}</p>
+          </div>
+          <div className="w-full   bg-white rounded-md shadow p-2   pb-2">
+            <p className="text-xs text-slate-400">Price</p>
+            <p>{formatCurrency(item.price)}</p>
+          </div>
+          <div className="w-full   bg-white rounded-md shadow p-2   pb-2">
+            <p className="text-xs text-slate-400">Barcode</p>
+            <p>{item.barcode ? item.barcode : "None"}</p>
+          </div>
+          <div className="w-full   bg-white rounded-md shadow p-2   pb-2">
+            <p className="text-xs text-slate-400">Character</p>
+            <p>{item.character ? item.character : "None"}</p>
+          </div>
+          <div className="w-full   bg-white rounded-md shadow p-2   pb-2">
+            <p className="text-xs text-slate-400">Product ID</p>
+            <p>{item.product_id ? item.product_id : "None"}</p>
+          </div>
         </div>
       </div>
     </div>

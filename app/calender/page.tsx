b1c050babd870a5b51825fdfc94407b193fbd648
alexanderscher <alexcscher@gmail.com>
2024-03-18 16:07:04 -0700

    update

--- app/calender/page.tsx
+++ app/calender/page.tsx
@@ -3,13 +3,12 @@ import { getReceipts } from "@/app/lib/receiptsDB";
 import { Receipt } from "@/types/receiptTypes";
 import React from "react";
 
-// const receipt = async () => {
-//   const receipts = await getReceipts();
-//   return receipts as Receipt[];
-// };
+const receipt = async () => {
+  const receipts = await getReceipts();
+  return receipts as Receipt[];
+};
 
 export default async function CalenderPage() {
-  // const receipts = await receipt();
-  // return <Calender receipts={receipts} />;
-  return <div>Calender</div>;
+  const receipts = await receipt();
+  return <Calender receipts={receipts} />;
 }

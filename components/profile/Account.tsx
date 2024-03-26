e6bebd65fe6b0dded49cf90ef841e073e9a1b63b
alexanderscher <alexcscher@gmail.com>
2024-03-26 16:02:03 -0700

    update

--- components/profile/Account.tsx
+++ components/profile/Account.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { changeEmail } from "@/actions/settings";
+import { changeEmail, changePassword } from "@/actions/settings";
 import RegularButton from "@/components/buttons/RegularButton";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
-import { EmailSchema } from "@/schemas";
+import { EmailSchema, PasswordSchema } from "@/schemas";
 import { Session } from "@/types/AppTypes";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
@@ -17,8 +17,6 @@ interface Props {
 }
 
 const Account = ({ session }: Props) => {
-  console.log(session);
-
   return (
     <div className="flex gap-6 ">
       <div className="bg-white min-w-[200px] rounded shadow p-8 flex flex-col gap-6">
@@ -36,33 +34,7 @@ const Account = ({ session }: Props) => {
 
         <div className="flex flex-col gap-4 ">
           <PersonalInformation session={session} />
-          {session?.user.isOAuth === false && (
-            <div className="bg-white rounded-md shadow  w-full flex flex-col p-8 gap-4">
-              <h1>Change Password</h1>
-              <div className="w-full">
-                <p className="text-sm text-slate-300">Current Password</p>
-                <input
-                  className="w-full border-[1px] border-slate-300 rounded-md h-[40px] p-2"
-                  type="text"
-                />
-              </div>
-              <div className="w-full">
-                <p className="text-sm text-slate-300">New Password</p>
-                <input
-                  className="w-full border-[1px] border-slate-300 rounded-md h-[40px] p-2"
-                  type="text"
-                />
-              </div>
-              <RegularButton
-                type="submit"
-                styles=" bg-black border-black text-white h-[40px]
-            
-            "
-              >
-                <p className="text-xs">Save Changes</p>
-              </RegularButton>
-            </div>
-          )}
+          <Password />
         </div>
       </div>
     </div>
@@ -100,6 +72,7 @@ const PersonalInformation = ({ session }: Props) => {
 
           if (data?.success) {
             setSuccess(data.success);
+            setError("");
           }
         })
         .catch(() => setError("Something went wrong!"));
@@ -160,6 +133,91 @@ const PersonalInformation = ({ session }: Props) => {
             </RegularButton>
             <FormError message={error} />
             <FormSuccess message={success} />
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+const Password = () => {
+  const [error, setError] = React.useState("");
+  const [success, setSuccess] = React.useState("");
+  const form = useForm<z.infer<typeof PasswordSchema>>({
+    resolver: zodResolver(PasswordSchema),
+    defaultValues: {
+      password: undefined,
+      newPassword: undefined,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof PasswordSchema>) => {
+    startTransition(() => {
+      changePassword(values)
+        .then((data) => {
+          if (data?.error) {
+            setError(data.error);
+          }
+
+          if (data?.success) {
+            setSuccess(data.success);
+            setError("");
+          }
+        })
+        .catch(() => setError("Something went wrong!"));
+    });
+  };
+  return (
+    <div className="bg-white rounded-md shadow  w-full justify-center">
+      <Formik
+        initialValues={{
+          password: "",
+          newPassword: "",
+        }}
+        onSubmit={(values) => {
+          if (values.password !== "" && values.newPassword !== "") {
+            onSubmit(values);
+          }
+        }}
+      >
+        {({
+          handleSubmit,
+
+          values,
+          handleChange,
+        }) => (
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+            <div className="bg-white rounded-md shadow  w-full flex flex-col p-8 gap-4">
+              <h1>Change Password</h1>
+              <div className="w-full">
+                <p className="text-sm text-slate-300">Current Password</p>
+                <input
+                  value={values.password}
+                  onChange={handleChange("password")}
+                  className="w-full border-[1px] border-slate-300 rounded-md h-[40px] p-2"
+                  type="text"
+                />
+              </div>
+              <div className="w-full">
+                <p className="text-sm text-slate-300">New Password</p>
+                <input
+                  value={values.newPassword}
+                  onChange={handleChange("newPassword")}
+                  className="w-full border-[1px] border-slate-300 rounded-md h-[40px] p-2"
+                  type="text"
+                />
+              </div>
+              <RegularButton
+                type="submit"
+                styles=" bg-black border-black text-white h-[40px]
+            
+            "
+              >
+                <p className="text-xs">Save Changes</p>
+              </RegularButton>
+              <FormError message={error} />
+              <FormSuccess message={success} />
+            </div>
           </form>
         )}
       </Formik>

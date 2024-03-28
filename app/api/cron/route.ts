3fd021a7195fe8aac3dace48c029c0e53c43fa10
alexanderscher <alexcscher@gmail.com>
2024-03-28 15:46:42 -0700

    update

---
+++ app/api/cron/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const result = "Helo, World! This is CRON route.";
+
+  return NextResponse.json({ data: result });
+}

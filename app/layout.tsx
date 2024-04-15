1d57022e241aeda70678b3bcfc25e9254ddc63b3
alexanderscher <alexcscher@gmail.com>
2024-04-15 16:16:16 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 import localFont from "next/font/local";
-import { Providers } from "@/components/providers/SessionProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const myFont = localFont({
   src: "../font/SuisseIntl-Medium.woff",
@@ -16,18 +17,19 @@ export const metadata: Metadata = {
   // themeColor: "#ecd96f",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="en">
       <body className={` ${myFont.className}`}>
-        <Providers>
+        <SessionProvider session={session}>
           <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           <main className="bg-[#e2f1e2]">{children}</main>
-        </Providers>
+        </SessionProvider>
         <Toaster />
       </body>
     </html>

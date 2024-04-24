40f0c586162f63bcbcbd233d40c37f4d34a27196
alexanderscher <alexcscher@gmail.com>
2024-04-24 16:28:06 -0700

    update

---
+++ actions/email/sendInvite.ts
@@ -0,0 +1,24 @@
+"use server";
+import { auth } from "@/auth";
+import { resend } from "@/lib/mail";
+import { Session } from "@/types/AppTypes";
+
+export const sendInvite = async (email: string): Promise<any> => {
+  const session = (await auth()) as Session;
+  const userId = session?.user?.id as string;
+  try {
+    await resend.emails.send({
+      from: "noreply@cubbie.io",
+      to: "alexcscher@gmail.com",
+      subject: `Join ${session.user.name} on Cubbie!`,
+      html: `<p>
+      You've been invited to join Cubbie. Click the link below to sign up.
+
+      http://cubbie.io/auth/register
+</p>`,
+    });
+  } catch (error) {
+    console.error("Failed to send invite:", error);
+    return { error: "An error occurred while sending the invite" };
+  }
+};
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex border-slate-300 h-9 w-full rounded-lg border bg-transparent px-3 py-1 text-sm  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus:border-emerald-900 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex border-slate-300 h-9 w-full rounded border bg-transparent px-3 py-1 text-sm  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus:border-emerald-900 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}

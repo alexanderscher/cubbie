96124d26e39b3a43769a0d6fffffd99726c9fa24
alexanderscher <alexcscher@gmail.com>
2024-05-13 21:10:15 -0700

    update

--- lib/getPolicy.ts
+++ lib/getPolicy.ts
@@ -2,26 +2,41 @@
 import { auth } from "@/auth";
 import prisma from "@/prisma/client";
 import { Session } from "@/types/Session";
+import { unstable_cache } from "next/cache";
+
+function getDynamicCacheKey(userId: string) {
+  return [`returns_user_${userId}`];
+}
 
 export const getPolicy = async () => {
-  try {
-    const session = (await auth()) as Session;
-    const userId = session?.user?.id as string;
-    if (!userId) {
-      return { error: "Unauthorized" };
-    }
+  const session = (await auth()) as Session;
+  const userId = session?.user?.id as string;
 
-    const items = await prisma.returns.findMany({
-      where: {
-        userId,
-      },
-      orderBy: {
-        created_at: "desc",
-      },
-    });
-    return { items };
-  } catch (error) {
-    console.error(error);
-    return { error: "An error occurred" };
-  }
+  const dynamicKey = getDynamicCacheKey(userId);
+  return unstable_cache(
+    async (userId) => {
+      try {
+        const session = (await auth()) as Session;
+        const userId = session?.user?.id as string;
+        if (!userId) {
+          return { error: "Unauthorized" };
+        }
+
+        const items = await prisma.returns.findMany({
+          where: {
+            userId,
+          },
+          orderBy: {
+            created_at: "desc",
+          },
+        });
+        return { items };
+      } catch (error) {
+        console.error(error);
+        return { error: "An error occurred" };
+      }
+    },
+    dynamicKey,
+    { tags: dynamicKey, revalidate: 60 }
+  )(userId);
 };

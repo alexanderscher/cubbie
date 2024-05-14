cc424e37c025da2135d4c1d00ea8784ea929eee1
alexanderscher <alexcscher@gmail.com>
2024-05-13 18:19:55 -0700

    update

---
+++ actions/return-policy/returns.ts
@@ -0,0 +1,69 @@
+"use server";
+import { auth } from "@/auth";
+import prisma from "@/prisma/client";
+import { Session } from "@/types/Session";
+
+export const createReturnPolicy = async (name: string, days: number) => {
+  try {
+    const session = (await auth()) as Session;
+    const userId = session?.user?.id as string;
+    if (!userId) {
+      return { error: "Unauthorized" };
+    }
+
+    const item = await prisma.returns.create({
+      data: {
+        store: name,
+        days,
+        User: { connect: { id: userId } },
+      },
+    });
+    return { item };
+  } catch (error) {
+    console.error(error);
+    return { error: "An error occurred" };
+  }
+};
+
+export const updatePolicy = async (id: number, name: string, days: number) => {
+  try {
+    const session = (await auth()) as Session;
+    const userId = session?.user?.id as string;
+    if (!userId) {
+      return { error: "Unauthorized" };
+    }
+
+    const item = await prisma.returns.update({
+      where: {
+        id,
+      },
+      data: {
+        store: name,
+        days,
+      },
+    });
+    return { item };
+  } catch (error) {
+    console.error(error);
+    return { error: "An error occurred" };
+  }
+};
+
+export const deletePolicy = async (id: number) => {
+  try {
+    const session = (await auth()) as Session;
+    const userId = session?.user?.id as string;
+    if (!userId) {
+      return { error: "Unauthorized" };
+    }
+
+    const item = await prisma.returns.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return { error: "An error occurred" };
+  }
+};
--- components/returns/Returns.tsx
+++ components/returns/Returns.tsx
@@ -1,5 +1,8 @@
 "use client";
 import RegularButton from "@/components/buttons/RegularButton";
+import SubmitButton from "@/components/buttons/SubmitButton";
+import { FormError } from "@/components/form-error";
+import Loading from "@/components/Loading/Loading";
 import { ModalOverlay } from "@/components/overlays/ModalOverlay";
 import { CreateProject } from "@/components/project/CreateProject";
 import SearchBar from "@/components/search/SearchBar";
@@ -44,7 +47,7 @@ const Returns = () => {
       </div>
       {createNew && (
         <ModalOverlay onClose={() => setCreateNew(false)}>
-          <CreateProject setAddProjectOpen={setCreateNew} />
+          <AddReturnPolicy />
         </ModalOverlay>
       )}
     </div>
@@ -52,3 +55,70 @@ const Returns = () => {
 };
 
 export default Returns;
+
+const AddReturnPolicy = () => {
+  return (
+    <div className="">
+      <div className="flex justify-between items-center border-b border-emerald-900 px-6 py-3  rounded-t-lg">
+        <h3 className="text-md text-emerald-900">Create Return Policy</h3>
+        <button
+          type="button"
+          className="text-emerald-900"
+          // onClick={() => setAddProjectOpen(false)}
+        >
+          <span className="text-2xl">&times;</span>
+        </button>
+      </div>
+      <div className="p-6 flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
+          <div className="space-y-4">
+            <div>
+              <p className="text-xs text-emerald-900 mb-2">Store name*</p>
+              <input
+                type="text"
+                name="description"
+                // value={project.name}
+                // onChange={(e) =>
+                //   setProject({ ...project, name: e.target.value })
+                // }
+                className="w-full p-2 border-[1px] rounded border-emerald-900 focus:outline-none"
+              />
+              {/* {error && <p className="text-orange-900 text-xs">{error}</p>} */}
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <div>
+              <p className="text-xs text-emerald-900 mb-2">
+                Number of days until return
+              </p>
+              <input
+                type="text"
+                name="description"
+                // value={project.name}
+                // onChange={(e) =>
+                //   setProject({ ...project, name: e.target.value })
+                // }
+                className="w-full p-2 border-[1px] rounded border-emerald-900 focus:outline-none"
+              />
+              {/* {error && <p className="text-orange-900 text-xs">{error}</p>} */}
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <SubmitButton
+              type="button"
+              // disabled={!project.name}
+              // handleClick={handleSubmit}
+            >
+              <p className="text-xs">Save Policy</p>
+            </SubmitButton>
+          </div>
+
+          {/* {uploadError && <FormError message={uploadError} />} */}
+        </div>
+        {/* {isPending && <Loading loading={isPending} />} */}
+      </div>
+    </div>
+  );
+};

72e7f60e9aac6f57b031b5d3e9ff4d3e9bf70237
alexanderscher <alexcscher@gmail.com>
2024-05-14 19:25:59 -0700

    update

--- app/returns/page.tsx
+++ app/returns/page.tsx
@@ -2,12 +2,13 @@ import { SearchReceiptProvider } from "@/components/context/SearchReceiptContext
 import Returns from "@/components/returns/Returns";
 import PageWrapper from "@/components/wrapper/PageWrapper";
 import { getPolicy } from "@/lib/getPolicy";
+import { ReturnType } from "@/types/Return";
 
 import { Suspense } from "react";
 
 const getReturnPolicy = async () => {
   const returns = await getPolicy();
-  return returns.items;
+  return returns.items as ReturnType[];
 };
 
 export default async function ReceiptPage() {

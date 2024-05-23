14f2268ed79a348e3a8c911774a546fb0490f551
alexanderscher <alexcscher@gmail.com>
2024-05-23 14:33:30 -0700

    update

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -14,13 +14,13 @@ const ManagePlan = async () => {
       <div className="flex flex-col items-center">
         {/* <Suspense fallback={<div>Loading...</div>}> */}
 
-        <div className="w-full max-w-[800px]">
+        <div className="w-full max-w-[750px]">
           <div className="w-full flex items-start">
             <h1 className="text-2xl font-semibold text-center my-8 text-emerald-900">
               Manage plan
             </h1>
           </div>
-          <div className="flex flex-col  gap-6">
+          <div className="flex flex-col  gap-6 mb-[200px]">
             {prices &&
               prices.map((price: any) => (
                 <PricingCard price={price} key={price.id} session={session} />

85e92ab2b5fc252b8f21c6617eb6d232a24dfdc9
alexanderscher <alexcscher@gmail.com>
2024-06-05 14:39:51 -0500

    update

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -10,19 +10,16 @@ export async function POST(request: Request) {
         {
           id: 1,
           name: "Free Plan",
-          description: "A free plan with limited features",
           price: 0,
         },
         {
           id: 2,
           name: "All Projects Plan",
-          description: "A plan that covers all projects",
           price: 0,
         },
         {
           id: 3,
           name: "Individual Projects Plan",
-          description: "A plan for individual projects",
           price: 0,
         },
       ],

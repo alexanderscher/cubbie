0f2d21a42391530637465b044e9c1e269a310c2e
alexanderscher <alexcscher@gmail.com>
2024-06-11 15:09:16 -0700

    update

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -43,43 +43,37 @@ const ManagePlan = async () => {
             </h1>
             <div className="flex flex-col gap-6">
               <div className="bg-white w-full shadow rounded-lg p-8 flex flex-col gap-3">
-                <h1>Upgrading</h1>
+                <h1 className="text-orange-600">Upgrading</h1>
                 <p>
-                  You can upgrade your plan at anytime by clicking the “Purchase
-                  now” or “14-day free trial” buttons. If you are upgrading from
-                  the free plan, you will need a valid credit card to start the
-                  trial. Your credit card will be charged only after the trial
-                  period ends.
+                  You can upgrade your subscription at any time through Stripe
+                  by clicking on either the &quot;Subscribe&quot; or
+                  &quot;14-day free trial&quot; buttons. If you&quot;re
+                  transitioning from the free plan, you&quot;ll need to enter a
+                  valid credit card to begin your trial. Your card will only be
+                  charged after the trial period concludes.
                 </p>
               </div>
               <div className="bg-white w-full shadow rounded-lg p-8 flex flex-col gap-3">
-                <h1>Downgrading</h1>
+                <h1 className="text-orange-600">Downgrading</h1>
                 <p>
-                  To downgrade your plan, you will need to meet the limits of
-                  the lower plan to which you wish to downgrade. A downgrade
-                  button will only be available to click on if you meet the plan
-                  limits for a lower tier. You can find a list of limits for all
-                  plans available here. When downgrading to the Cubbie Free
-                  plan, you’ll have access to your current plan until the next
-                  billing cycle. At the end of the billing cycle, your account
-                  will be downgraded, and your credit card will no longer be
-                  charged. When downgrading from Cubbie Advanced to Cubbie
-                  Limited, your plan will be downgraded immediately and you’ll
-                  get credit which will be applied to future payments. A
-                  downgrade button will only be available to click on if you
-                  meet the plan limits for the lower tier. You can find a list
-                  of limits for all plans available here.
+                  To downgrade your plan in Cubbie, you must first align with
+                  the usage limits of the desired lower tier. You will be unable
+                  to downgrade unless these limits are met. When moving to the
+                  Cubbie Free plan, you continue on your current plan until the
+                  next billing cycle, after which your plan changes, and no
+                  further charges are applied to your credit card. For
+                  downgrades from Cubbie Advanced to Cubbie Limited, the change
+                  is immediate, and any resulting credit will be applied to
+                  future payments.
                 </p>
               </div>
               <div className="bg-white w-full shadow rounded-lg p-8 flex flex-col gap-3">
-                <h1>Invitations</h1>
+                <h1 className="text-orange-600">Invitations</h1>
                 <p>
                   If you are subscribed to a plan, you can invite other users to
                   join your project, allowing them to access the benefits of
                   your subscription. Similarly, if someone else has a
-                  subscription, they can invite you to their project. To send or
-                  accept an invitation, navigate to the ‘Project Settings’ page
-                  and use the ‘Invite Members’ option.
+                  subscription, they can invite you to their project.
                 </p>
                 <p>
                   Invitations are managed through your project dashboard. An
@@ -87,21 +81,20 @@ const ManagePlan = async () => {
                 </p>
               </div>
               <div className="bg-white w-full shadow rounded-lg p-8 flex flex-col gap-3">
-                <h1>Cancellation</h1>
+                <h1 className="text-orange-600">Cancellation</h1>
                 <p>
-                  If you no longer wish to use a paid Cubbie plan, you have a
-                  few options. You can downgrade to the Cubbie Free plan. The
-                  downgrade button will only be available to click on if you
-                  meet the plan limits for the Cubbie Free plan. You can find a
-                  list of limits for all plans available here. Or you can delete
-                  your account and its associated data.
+                  If you no longer wish to use a paid Cubbie plan, you can
+                  downgrade to the Cubbie Free plan. The downgrade will only be
+                  available to click on if you meet the plan limits for the
+                  Cubbie Free plan. Or you can delete your account and its
+                  associated data.
                 </p>
               </div>
               <div className="bg-white w-full shadow rounded-lg p-8 flex flex-col gap-3">
-                <h1>Deleting account</h1>
+                <h1 className="text-orange-600">Deleting account</h1>
                 <p>
                   To permanently delete your account and remove all the data, go
-                  to the Manage Account settings section below. Once your
+                  to the user profile page in account settings. Once your
                   account is deleted, you will instantly lose access to your
                   account and the data cannot be recovered.
                 </p>
